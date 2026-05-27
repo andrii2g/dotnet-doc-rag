c9dc3e5dd358322f3cc4e2e5559d618af57ff1cd
Andrey <andrii@a2g.name>
2026-05-28 01:42:17 +0300

    added embeddings default constants

---
+++ src/DocRag.Core/Embeddings/EmbeddingDefaults.cs
@@ -0,0 +1,8 @@
+namespace DocRag.Core.Embeddings;
+
+public static class EmbeddingDefaults
+{
+    public const string Provider = "OpenAI";
+    public const string Model = "text-embedding-3-small";
+    public const int Dimensions = 1536;
+}
